94233b0810839e57cfd37ad1bf57fbea567cbc7f
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-14 22:59:08 -0500

    Fix MongoDB connection error handling

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const mongoURI = 'mongodb://localhost:27017/team-task-tracker';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err)); // Improved error logging
 
 app.get('/api/tasks', (req, res) => {
   res.send('GET request to the homepage');
