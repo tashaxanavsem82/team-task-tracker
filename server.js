37c09ccd5b24df8a97c0c686af44d8b22d5bb21f
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-14 22:57:04 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// Replace with your MongoDB connection string
+const mongoURI = 'mongodb://localhost:27017/team-task-tracker';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/api/tasks', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
